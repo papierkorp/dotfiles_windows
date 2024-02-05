e9414544215c2f6747e4c5a7376c4d62377404fa
Markus Schiller <m.schiller@ymail.com>
2024-02-05 14:13:35 +0100

    feat: update

--- pull_all.sh
+++ pull_all.sh
@@ -2,78 +2,79 @@
 
 repos_work=("infrastructure" "leasone" "wiki")
 repos_privat=("cheatsheet" "lanparty" "blog" "THM" "horizon-flow" "godot/beastmaster")
-repos_worklab=("infrastructure" "leasone" "Testrepo")
+# repos_worklab=("infrastructure" "leasone" "Testrepo")
 system_info=$(uname -a)
 
+echo "System: $system_info"
 
 if [[ $system_info == *"MINGW64"* ]]; then
-  echo "executing gitbash script.."
+    echo "executing gitbash script.."
 
-  for repo in "${repos_work[@]}"
-  do
-    echo "Pulling Ops - $repo..."
-    cd "C:/develop/space/$repo"
+    for repo in "${repos_work[@]}"
+    do
+        echo "Pulling Ops - $repo..."
+        cd "C:/develop/space/$repo" || return
+        git pull
+    done
+
+    # for repo in "${repos_worklab[@]}"
+    # do
+    #   echo "Pulling Lab - $repo..."
+    #   cd "C:/develop/labspace/$repo"
+    #   git pull
+    # done
+
+
+
+    for repo in "${repos_privat[@]}"
+    do
+        echo "Pulling Private - $repo..."
+        cd "C:/develop/privat/$repo" || return
+        git pull
+    done
+
+    echo "Pulling SublimeText"
+    cd "C:/Users/Markus/AppData/Roaming/Sublime Text/Packages/User" || return
     git pull
-  done
 
-  # for repo in "${repos_worklab[@]}"
-  # do
-  #   echo "Pulling Lab - $repo..."
-  #   cd "C:/develop/labspace/$repo"
-  #   git pull
-  # done
-
-
-
-  for repo in "${repos_privat[@]}"
-  do
-    echo "Pulling Private - $repo..."
-    cd "C:/develop/privat/$repo"
+    echo "Pulling todo\`s.."
+    cd "C:/develop/sirconic/todo" || return
     git pull
-  done
-
-  echo "Pulling SublimeText"
-  cd "C:/Users/Markus/AppData/Roaming/Sublime Text/Packages/User"
-  git pull
-
-  echo "Pulling todo\`s.."
-  cd "C:/develop/sirconic/todo"
-  git pull
 
 elif [[ $system_info == *"Linux"* ]]; then
-  echo "executing wsl script.."
+    echo "executing wsl script.."
 
-  for repo in "${repos_work[@]}"
-  do
-    echo "Pulling $repo..."
-    cd "/mnt/c/develop/space/$repo"
+    for repo in "${repos_work[@]}"
+    do
+        echo "Pulling $repo..."
+        cd "/mnt/c/develop/space/$repo" || return
+        git pull
+    done
+
+    # for repo in "${repos_worklab[@]}"
+    # do
+    #   echo "Pulling $repo..."
+    #   cd "/mnt/c/develop/labspace/$repo"
+    #   git pull
+    # done
+
+    for repo in "${repos_privat[@]}"
+    do
+        echo "Pulling $repo..."
+        cd "/mnt/c/develop/privat/$repo" || return
+        git pull
+    done
+
+    echo "Pulling SublimeText"
+    cd "/mnt/c/Users/Markus/AppData/Roaming/Sublime Text/Packages/User" || return
     git pull
-  done
 
-  # for repo in "${repos_worklab[@]}"
-  # do
-  #   echo "Pulling $repo..."
-  #   cd "/mnt/c/develop/labspace/$repo"
-  #   git pull
-  # done
-
-  for repo in "${repos_privat[@]}"
-  do
-    echo "Pulling $repo..."
-    cd "/mnt/c/develop/privat/$repo"
+    echo "Pulling todo\`s.."
+    cd "/mnt/c/develop/sirconic/todo" || return
     git pull
-  done
 
-  echo "Pulling SublimeText"
-  cd "/mnt/c/Users/Markus/AppData/Roaming/Sublime Text/Packages/User"
-  git pull
-
-  echo "Pulling todo\`s.."
-  cd "/mnt/c/develop/sirconic/todo"
-  git pull
-
-  dotf pull #pull dotfiles
-  dotfw pull #pull windows dotfiles
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" pull #pull dotfiles
+    /usr/bin/git --git-dir="/mnt/c/Users/Markus/.dotfiles" --work-tree="/mnt/c/Users/Markus" pull #pull windows dotfiles
 else
-  echo "Unknown system."
+    echo "Unknown system."
 fi
--- push_all.sh
+++ push_all.sh
@@ -5,71 +5,72 @@ repos_privat=("cheatsheet" "lanparty" "blog" "THM" "horizon-flow" "godot/beastma
 
 # Check if the system_info contains "MINGW64" to identify Windows
 if [[ $system_info == *"MINGW64"* ]]; then
-  echo "executing gitbash script.."
+    echo "executing gitbash script.."
 
-  for repo in "${repos_privat[@]}"
-  do
-    echo "Pushing Private - $repo..."
-    cd "C:/develop/privat/$repo"
+    for repo in "${repos_privat[@]}"
+    do
+        echo "Pushing Private - $repo..."
+        cd "C:/develop/privat/$repo" || return
+        git add -u
+        git commit -m "pushed with daily script"
+        git push
+        echo -e "Push of $repo successfull. \n \n"
+    done
+
+    echo "Pushing todo\`s.."
+    cd "C:/develop/sirconic/todo" || return
     git add -u
     git commit -m "pushed with daily script"
     git push
-    echo -e "Push of $repo successfull. \n \n"
-  done
-
-  echo "Pushing todo\`s.."
-  cd "C:/develop/sirconic/todo"
-  git add -u
-  git commit -m "pushed with daily script"
-  git push
-  echo "Push of todo\`s successfull."
+    echo "Push of todo\`s successfull."
 
 
-  echo "Pushing SublimeText"
-  cd "C:/Users/Markus/AppData/Roaming/Sublime Text/Packages/User"
-  git add -u
-  git commit -m "pushed with daily script"
-  git push
-  echo "Push of Sublime Text successfull."
+    echo "Pushing SublimeText"
+    cd "C:/Users/Markus/AppData/Roaming/Sublime Text/Packages/User" || return
+    git add -u
+    git commit -m "pushed with daily script"
+    git push
+    echo "Push of Sublime Text successfull."
 
 elif [[ $system_info == *"Linux"* ]]; then
-  echo "executing wsl script.."
+    echo "executing wsl script.."
 
-  for repo in "${repos_privat[@]}"
-  do
-    echo "Pushing $repo..."
-    cd "/mnt/c/develop/privat/$repo"
+    for repo in "${repos_privat[@]}"
+    do
+        echo "Pushing $repo..."
+        cd "/mnt/c/develop/privat/$repo" || return
+        git add -u
+        git commit -m "pushed with daily script"
+        git push
+        echo -e "Push of $repo successfull. \n \n"
+    done
+
+    echo "Pushing todo\`s.."
+    cd "/mnt/c/develop/sirconic/todo" || return
     git add -u
     git commit -m "pushed with daily script"
     git push
-    echo -e "Push of $repo successfull. \n \n"
-  done
-
-  echo "Pushing todo\`s.."
-  cd "/mnt/c/develop/sirconic/todo"
-  git add -u
-  git commit -m "pushed with daily script"
-  git push
-  echo "Push of todo\`s successfull."
+    echo "Push of todo\`s successfull."
 
 
-  echo "Pushing SublimeText"
-  cd "/mnt/c/Users/Markus/AppData/Roaming/Sublime Text/Packages/User"
-  git add -u
-  git commit -m "pushed with daily script"
-  git push
-  echo "Push of Sublime Text successfull."
+    echo "Pushing SublimeText"
+    cd "/mnt/c/Users/Markus/AppData/Roaming/Sublime Text/Packages/User" || return
+    git add -u
+    git commit -m "pushed with daily script"
+    git push
+    echo "Push of Sublime Text successfull."
 
-  echo "Pushing Dotfiles"
-  dotf add -u
-  dotf commit -m "pushed with daily script"
-  dotf push
 
-  dotfw add -u
-  dotfw commit -m "pushed with daily script"
-  dotfw push
+    echo "Pushing Dotfiles"
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" add -u
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" commit -m "pushed with daily script"
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" push
+
+    /usr/bin/git --git-dir="/mnt/c/Users/Markus/.dotfiles" --work-tree="/mnt/c/Users/Markus" add -u
+    /usr/bin/git --git-dir="/mnt/c/Users/Markus/.dotfiles" --work-tree="/mnt/c/Users/Markus" commit -m "pushed with daily script"
+    /usr/bin/git --git-dir="/mnt/c/Users/Markus/.dotfiles" --work-tree="/mnt/c/Users/Markus" push
 else
-  echo "Unknown system."
+    echo "Unknown system."
 fi
 
 
